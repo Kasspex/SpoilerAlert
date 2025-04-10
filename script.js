5de7eb67de38aa06e403679b5bcd30fefd55d8dc
Kasspex <feliphomezparda@gmail.com>
2025-04-10 12:53:09 -0500

    primer commit a mi rama

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("bttnEnviar").addEventListener('click', function(){
+    const inputs = document.getElementsByClassName("formulario");
+    const texto = inputs.value.trim()
+
+
+})
