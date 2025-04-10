2271541f8d8a4a938de4bf96386a9d84a7a5bfdd
Kasspex <feliphomezparda@gmail.com>
2025-04-10 14:19:49 -0500

    Logica finalizada

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.getElementById("bttnEnviar").addEventListener('click', function(){
-    const inputs = document.getElementsByClassName("formulario");
-    const texto = inputs.value.trim()
+document.getElementById("form").addEventListener('submit', function(e){
+    e.preventDefault();
+    let inputs = Array.from(document.getElementsByClassName("formulario"));
+    let texto = []
+    for(let i = 0; i<inputs.length; i++){
+        texto.push(inputs[i].value.trim())
+    }
 
-
+    alert(`Te contactaremos pronto ${texto[0]}`)
+    document.getElementById("form").reset()
 })
